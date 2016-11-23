306d807cc6ba70ad272e913e9f2630d5866bafc4
Hikita Kamyshenko <nikita.kamyshenko@mail.ru>
2016-11-24 00:28:32 +0300

    Fix test

--- encryption/test/tests.js
+++ encryption/test/tests.js
@@ -3,12 +3,12 @@
  */
 var assert = chai.assert;
 
-describe("JuliusCaesar", function () {
+describe("Main", function () {
 
 
     describe("transitionInRange", function () {
         it("Return a number in the range.", function () {
-            assert.equal(transitionInRange(3,4), 3);
+            assert.equal(transitionInRange(3, 4), 3);
         });
 
         it("If the number exceeds the maximum value.", function () {
@@ -21,16 +21,22 @@ describe("JuliusCaesar", function () {
 
     });
 
-    it("Get index for mixed alphabet.", function () {
-        assert.equal(mixingIndex(33, 1), 0);
-    });
+    describe("JuliusCaesar", function () {
+        var cryp = createCryptographerJuliusCaesar();
+        it("Get index for mixed alphabet.", function () {
+            assert.equal(cryp.mixingIndex(33, 1), 0);
+        });
 
-    it("Encryption text.", function () {
-        assert.equal(encryptionText("абв_я", 1), "бвга_");
-    });
+        it("Encryption text.", function () {
+            assert.equal(cryp.encryptionText("абв_я", 1), "бвга_");
+        });
 
-    it("Decryption text.", function () {
-        assert.equal(encryptionText("бвга_", -1), "абв_я");
+        it("Decryption text.", function () {
+            assert.equal(cryp.encryptionText("бвга_", -1), "абв_я");
+        });
+    });
+    describe("PolubinskiSquare", function () {
+        //TODO Add Test for PolubinskiSquare.
     });
 
 });
